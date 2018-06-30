771b0052a8143fd02787d0e8a4b17bcea6da3132
SaiManikantaG <saig2304@gmail.com>
2018-06-30 01:39:20 -0500

    Future api setup for deployments

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,30 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 // api service
 @Injectable()
 export class ApiService {
-  messages = [];
-  users = [];
-  path = 'http://localhost:3000';
-  constructor(private http: HttpClient) {}
+	messages = [];
+	users = [];
+	path = environment.path;
+	constructor(private http: HttpClient) {}
 
-  getMessages(userId) {
-    this.http.get<any>(this.path + '/posts/' + userId).subscribe(res => {
-      this.messages = res;
-    });
-  }
+	getMessages(userId) {
+		this.http.get<any>(this.path + '/posts/' + userId).subscribe(res => {
+			this.messages = res;
+		});
+	}
 
-  postMessages(message) {
-    this.http.post(this.path + '/posts', message).subscribe(res => {});
-  }
+	postMessages(message) {
+		this.http.post(this.path + '/posts', message).subscribe(res => {});
+	}
 
-  getUsers() {
-    this.http.get<any>(this.path + '/users').subscribe(res => {
-      this.users = res;
-    });
-  }
+	getUsers() {
+		this.http.get<any>(this.path + '/users').subscribe(res => {
+			this.users = res;
+		});
+	}
 
-  getProfile(id) {
-    return this.http.get(this.path + '/profile/' + id);
-  }
+	getProfile(id) {
+		return this.http.get(this.path + '/profile/' + id);
+	}
 }
--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,40 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class AuthService {
-  messages = [];
-  path = 'http://localhost:3000/auth';
+	messages = [];
+	path = environment.path + '/auth';
 
-  TOKEN_KEY = 'token';
+	TOKEN_KEY = 'token';
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {}
 
-  getToken() {
-    return localStorage.getItem(this.TOKEN_KEY);
-  }
+	getToken() {
+		return localStorage.getItem(this.TOKEN_KEY);
+	}
 
-  get isAuthenticated() {
-    return !!localStorage.getItem(this.TOKEN_KEY);
-  }
+	get isAuthenticated() {
+		return !!localStorage.getItem(this.TOKEN_KEY);
+	}
 
-  logout() {
-    localStorage.removeItem(this.TOKEN_KEY);
-  }
+	logout() {
+		localStorage.removeItem(this.TOKEN_KEY);
+	}
 
-  registerUser(registerData) {
-    this.http.post<any>(this.path + '/register', registerData).subscribe(res => {
-      console.log(res);
-      this.saveToken(res.token);
-    });
-  }
-  loginUser(loginData) {
-    this.http.post<any>(this.path + '/login', loginData).subscribe(res => {
-      this.saveToken(res.token);
-    });
-  }
+	registerUser(registerData) {
+		this.http.post<any>(this.path + '/register', registerData).subscribe(res => {
+			console.log(res);
+			this.saveToken(res.token);
+		});
+	}
+	loginUser(loginData) {
+		this.http.post<any>(this.path + '/login', loginData).subscribe(res => {
+			this.saveToken(res.token);
+		});
+	}
 
-  saveToken(token) {
-    localStorage.setItem(this.TOKEN_KEY, token);
-  }
+	saveToken(token) {
+		localStorage.setItem(this.TOKEN_KEY, token);
+	}
 }
