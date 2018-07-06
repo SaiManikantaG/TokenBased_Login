a08317c2730270ec0bfb853da2b1a0e1033dc7a4
SaiManikantaG <saig2304@gmail.com>
2018-07-06 00:02:13 -0500

    FIle upload functionality changes, added missing folder

--- frontend/src/app/user/register.component.ts
+++ frontend/src/app/user/register.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import { AuthService } from '.././auth/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
 	selector: 'register',
 	templateUrl: 'register.component.html',
 })
 export class RegisterComponent {
+	path = 'http://localhost:3000';
 	registerData = {};
+	selectedFileProp = null;
 
-	constructor(private authService: AuthService) {}
-
+	constructor(private authService: AuthService, private http: HttpClient) {}
+	onFileSelected(event) {
+		this.selectedFileProp = <File>event.target.files[0];
+		console.log(event);
+	}
 	post() {
 		console.log(this.registerData);
 		this.authService.registerUser(this.registerData);
+		const formData = new FormData();
+		formData.append('image', this.selectedFileProp, this.selectedFileProp.name);
+		this.http.post(this.path + '/upload', formData).subscribe(res => {
+			console.log(res);
+		});
 	}
 }
